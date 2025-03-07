d7a7b16ba4525e070d48906f2597d36844875360
safalafas <safal@ismt.edu.np>
2025-03-07 14:46:08 -0500

    Add initial Terraform configuration and setup Husky pre-commit hooks

---
+++ infrastructure/main.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "chau0317-A09-RG"
+  location = "canadacentral"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "chau0317a09storage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
