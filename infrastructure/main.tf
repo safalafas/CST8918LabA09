0ebe884044a2a6a24dbafc7a9a9dfc45e6cc2ac9
safalafas <safal@ismt.edu.np>
2025-03-07 14:51:29 -0500

    fix: set Azure Storage Account replication type to LRS

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,4 +12,5 @@ resource "azurerm_storage_account" "storage" {
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
-  account_replication_type = 
+  account_replication_type = "LRS"
+}
